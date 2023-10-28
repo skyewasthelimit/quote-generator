0d6f736cc7d792573b666354aebc05f04b3e0cb0
Skye McLean <skyemclean87@gmail.com>
2023-10-28 10:59:53 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const RandomQuote = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await fetch('https://api.quotable.io/random');
+      const data = await response.json();
+
+      if (data && data.content && data.author) {
+        setQuote(data.content);
+        setAuthor(data.author);
+      }
+    } catch (error) {
+      console.error('Error fetching random quote:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
+  const handleNewQuoteClick = () => {
+    fetchRandomQuote();
+  };
+
+  const handleTweetClick = () => {
+    const tweetText = `"${quote}" - ${author}`;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+    
+    // Open a new window with the tweet intent
+    window.open(tweetUrl, '_blank');
+  };
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header">
+        <div id="quote-box">
+          <div id="text">"{quote}"</div>
+          <div id="author">-{author}</div>
+        </div>
+        <button id="new-quote" onClick={handleNewQuoteClick}>
+            New Quote
+          </button>
+          <button id="tweet" onClick={handleTweetClick}>Share on Twitter</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default RandomQuote;
